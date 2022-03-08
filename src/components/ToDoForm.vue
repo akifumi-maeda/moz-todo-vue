e10879015da26b3a3647fd7269ba06c0319258f9
Akifumi Maeda <akifumi_maeda@maede.co.jp>
2022-03-08 16:21:03 +0900

    Adding the new todo into our data

--- src/components/ToDoForm.vue
+++ src/components/ToDoForm.vue
@@ -20,6 +20,9 @@
     export default {
         methods: {
             onSubmit() {
+                if (this.label === "") {
+                    return;
+                }
                 this.$emit("todo-added", this.label);
             }
         },
